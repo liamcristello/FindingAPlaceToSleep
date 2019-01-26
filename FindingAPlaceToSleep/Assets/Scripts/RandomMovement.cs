3f6656c66d34d10a7a0c13e9f8b08fd8c6374f7d
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2019-01-26 10:29:14 -0500

    Rescaled environment and added roomba prototype

---
+++ FindingAPlaceToSleep/Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
