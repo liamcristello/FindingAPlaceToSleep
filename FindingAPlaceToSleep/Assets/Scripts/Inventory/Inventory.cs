cff11552415a6572006f99faf296f0b91c34991f
Liam Cristello <liamcristello@gmail.com>
2019-01-26 11:59:06 -0500

    Basic inventory script

---
+++ FindingAPlaceToSleep/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    // Number of item slots
+    public const int numItemSlots = 4;
+    /* 
+     * Int and item arrays used to store necessary inventory items
+     * and corresponding images. Each length is set to our   
+     * numItemSlots variable.s
+     */   
+    public Image[] itemImages = new Image[numItemSlots];
+    public Item[] items = new Item[numItemSlots];
+
+    // Add item to inventory.
+    public void AddItem(Item itemToAdd)
+    {
+        /*
+         * Loops through items until finding an empty slot,
+         * adds in and displays itemToAdd. Updates both items and
+         * itemImages arrays so item is displayed correctly.
+         */
+         for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] == null)
+            {
+                items[i] = itemToAdd;
+                itemImages[i].sprite = itemToAdd.sprite;
+                itemImages[i].enabled = true;
+                return;
+            }
+        }
+    }
+
+    // Remove item from inventory.
+    public void RemoveItem(Item itemToRemove)
+    {
+        /*
+         * Loops through items until finding itemToRemove,
+         * and removes it. Updates both items and
+         * itemImages arrays.
+         */
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] == itemToRemove)
+            {
+                items[i] = null;
+                itemImages[i].sprite = null;
+                itemImages[i].enabled = false;
+                return;
+            }
+        }
+    }
+}
---
+++ FindingAPlaceToSleep/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Creatable from the Asset menu.
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    // Sprite to display the item.
+    public Sprite sprite;
+}
