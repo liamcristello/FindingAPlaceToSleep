40da106e694e30efa5dce515a10dc4e19e54d557
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2019-01-27 14:51:29 -0500

    Added window assets in scene with opacity script

---
+++ FindingAPlaceToSleep/Assets/Scripts/SpriteOpacity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOpacity : MonoBehaviour
+{
+    SpriteRenderer spr;
+
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        spr.color = new Color(1f, 1f, 1f, .5f);
+    }
+}
